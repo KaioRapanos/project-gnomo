514708441565264640ccce7272fb52fe0e7561bb
KaioRapanos <kaiorapanos@gmail.com>
2024-05-27 20:36:15 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html'
-      },
-      output: {
-        assetFileNames: 'assets/[name][extname]'
-      }
-    },
-    assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg', '**/*.mp3']
-  }
+  plugins: [react()]
 })
